1a024e5a711ccc65cf9ecffdd78f626ca462a60f
MaHa-Work <114355529+MaHa-Work@users.noreply.github.com>
2023-02-13 14:00:22 -0500

    first commit

---
+++ src/main/java/net/revature/casestudymjh/CasestudyMjhApplication.java
@@ -0,0 +1,13 @@
+package net.revature.casestudymjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyMjhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyMjhApplication.class, args);
+	}
+
+}
